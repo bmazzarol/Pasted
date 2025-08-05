9a0290e257c670451c5627a7038f0b40fe9015f6
bmazzarol-bunnings <bmazzarol@bunnings.com.au>
2025-08-05 23:37:47 +0800

    feat: baby stages

--- Pasted.Sample/Examples.cs
+++ Pasted.Sample/Examples.cs
@@ -5,4 +5,5 @@ namespace Pasted.Sample;
 public static partial class Examples
 {
     // TODO: Add examples for Pasted
+    const string file1 = EmbeddedFiles.SampleText;
 }
--- Pasted/SourceGenerator.cs
+++ Pasted/SourceGenerator.cs
@@ -1,12 +1,7 @@
-using System.CodeDom.Compiler;
 using System.Text;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
-using SF = Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
-namespace Cutout;
+namespace Pasted;
 
 /// <summary>
 /// Source generator for Pasted embedded files.
@@ -14,8 +9,42 @@ namespace Cutout;
 #pragma warning disable RS1038
 [Generator]
 #pragma warning restore RS1038
-public sealed partial class SourceGenerator : IIncrementalGenerator
+public sealed class SourceGenerator : IIncrementalGenerator
 {
     /// <inheritdoc />
-    public void Initialize(IncrementalGeneratorInitializationContext context) { }
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // Collect all additional files (these are provided by MSBuild)
+        var embeddedFiles = context.AdditionalTextsProvider.Select((file, _) => file).Collect();
+
+        context.RegisterSourceOutput(
+            embeddedFiles,
+            (spc, files) =>
+            {
+                // TODO: Get the root namespace from analyzer config options
+                var ns = "Pasted";
+
+                foreach (var file in files)
+                {
+                    var name = Path.GetFileNameWithoutExtension(file.Path)
+                        .Replace('-', '_')
+                        .Replace('.', '_');
+                    var content = file.GetText(spc.CancellationToken)
+                        ?.ToString()
+                        .Replace("\"", "\\\"");
+                    var sb = new StringBuilder();
+                    sb.Append("namespace ").Append(ns).AppendLine(" {");
+                    sb.AppendLine("    internal static partial class EmbeddedFiles {");
+                    sb.Append("        public const string ")
+                        .Append(name)
+                        .Append(" = @\"")
+                        .Append(content)
+                        .AppendLine("\";");
+                    sb.AppendLine("    }");
+                    sb.AppendLine("}");
+                    spc.AddSource($"EmbeddedFiles.{name}.g.cs", sb.ToString());
+                }
+            }
+        );
+    }
 }
